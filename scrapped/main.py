0974aeb69cc1bf3a9c6cf6602b69258c50d30452
YeDragonFruit <agspring31@gmail.com>
2026-05-15 17:16:47 -0500

    Uncomment scipy constants and related calculations

--- scrapped/main.py
+++ scrapped/main.py
@@ -1,5 +1,5 @@
 import scipy
-# import scipy.constants as const
+import scipy.constants as const
 
 
 # A = Ampere
@@ -41,21 +41,21 @@ print("EP_0 =", EP_0)
 print("cc1 =", cc1)
 print("cc2 =", cc2)
 print("cc3 =", cc3)
-# print(scipy.__version__)
+print(scipy.__version__)
 
-#print(f"Speed of light (c): {const.c}")
-#print(f"Pi: {const.pi}")
+print(f"Speed of light (c): {const.c}")
+print(f"Pi: {const.pi}")
 
 # Best Practice: Use the direct attribute variables
-#epsilon_0 = const.epsilon_0
-#mu_0 = const.mu_0
+epsilon_0 = const.epsilon_0
+mu_0 = const.mu_0
 
-#print(f"Epsilon_0: {epsilon_0}")
-#print(f"Mu_0: {mu_0}")
+print(f"Epsilon_0: {epsilon_0}")
+print(f"Mu_0: {mu_0}")
 
 # Calculate Coulomb's constant
-#k_e = 1 / (4 * const.pi * epsilon_0)
-#print(f"Calculated k_e: {k_e}")
+k_e = 1 / (4 * const.pi * epsilon_0)
+print(f"Calculated k_e: {k_e}")
 
 
 
