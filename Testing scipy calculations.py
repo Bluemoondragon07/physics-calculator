4c6d7cd4f3e99a333435fa077e2300a77accd812
Bluemoondragon07 <bluemoondragon07@gmail.com>
2026-05-25 20:13:25 -0500

    test with a momentum calculation

--- Testing scipy calculations.py
+++ Testing scipy calculations.py
@@ -2,8 +2,30 @@ import scipy
 import scipy.constants as const
 
 
-choice = input("What would you like to calculate? \n (A) dot product \n (B) cross product \n")
+# things to test
+
+# 1. calculate momentum
+# --> p = mv; Fnet = ma -> m = Fnet/a --> p = Fnet/a * v
+# ----> variables: Fnet, m , a, v 
+#       user select which variables to input -> determine appropriate formula to use
+# include option for freefall, throwing/launching projectile from user-defined height 
+
+# basic momentum calculator 
+
+print("Welcome to momentum calculator.") 
+mass = input("If the object's mass is known, enter it (kg). Press [Enter] if unknown");
+velocity = input("Enter velocity (m/s). [Enter] to skip")
+
+if not mass and not velocity: # if already have mass and velocity, don't need to ask for these
+    acceleration = input("Enter acceleration (m/s^2). [Enter] to skip")
+    Fnet = input("Enter net force (N). [Enter] to skip")
 
 
-if choice == 'A': print("dot product")
-elif choice == 'B': print("cross product")
+if mass and velocity:
+    momentum = float(mass) * float(velocity)
+elif Fnet and acceleration and velocity:
+    mass = float(Fnet) / float(acceleration)
+    momentum = mass * float(velocity)   
+   
+print(f"Momentum (p) = {momentum} kg*m/s")
+
