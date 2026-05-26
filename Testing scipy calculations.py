0bd9b9c23b15fa8556ef31e22e24f320fbe71670
Bluemoondragon07 <bluemoondragon07@gmail.com>
2026-05-25 20:02:44 -0500

    added file for test calculations

---
+++ Testing scipy calculations.py
@@ -0,0 +1,9 @@
+import scipy
+import scipy.constants as const
+
+
+choice = input("What would you like to calculate? \n (A) dot product \n (B) cross product \n")
+
+
+if choice == 'A': print("dot product")
+elif choice == 'B': print("cross product")
